fa6b3e734b76e34ceaf2451ff48f7662e971e0b3
vicky <dogovicky05@gmail.com>
2025-03-12 11:38:40 +0300

    Configured services with database connections

---
+++ Microservices-Backend/Api-Gateway/src/test/java/com/capricon/Api_Gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capricon.Api_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
